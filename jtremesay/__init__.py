de1cfea124513d359bd4bbd17833ff96b51ad21f
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2023-06-01 21:18:36 +0200

    simplify !

--- jtremesay/__init__.py
+++ jtremesay/__init__.py
@@ -1,6 +1,50 @@
+import datetime
+import importlib
+from pathlib import Path
 import os
+import re
 
-from flask import Flask
+from flask import Flask, render_template, abort, url_for, current_app
+from jinja2 import Template
+from jinja2.exceptions import TemplateNotFound
+
+re_post_entry = re.compile(r"(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})_(?P<slug>\w+)\.html")
+
+def get_posts_data():
+    posts_path = importlib.resources.files("jtremesay.templates.blog")
+    posts = []
+    for post_entry in posts_path.iterdir():
+        if not post_entry.is_file():
+            continue
+
+        match = re_post_entry.match(post_entry.name)
+        if not match:
+            continue
+
+        groups = match.groupdict()
+        year = int(groups["year"])
+        month = int(groups["month"])
+        day = int(groups["day"])
+        slug = groups["slug"]
+
+        ast = current_app.jinja_env.parse(Path(post_entry).read_text())
+        for node in ast.body:
+            if node.__class__.__name__ == "Block" and node.name == "title":
+                title = node.body[0].nodes[0].data
+                break
+        else:
+            continue
+
+        post = {
+            "pubdate": datetime.date(year, month, day),
+            "slug": slug,
+            "title": title,
+            "url": url_for("view_post", year=year, month=month, day=day, slug=slug)
+        
+        }
+        posts.append(post)
+
+    return posts
 
 
 def create_app(test_config=None):
@@ -19,12 +63,24 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    @app.route("/", defaults={"page": "index"})
+    @app.route("/<string:page>")
+    def view_page(page: str) -> str:
+        try:
+            return render_template(f"pages/{page}.html")
+        except TemplateNotFound:
+            abort(404)
 
-    from . import blog
-    app.register_blueprint(blog.bp, url_prefix="/blog")
+    @app.route("/blog/")
+    def list_posts() -> str:
+        posts = get_posts_data()
+        return render_template("list_posts.html", posts=posts)
 
-    from . import page
-    app.register_blueprint(page.bp)
-    app.add_url_rule('/', endpoint='page.page')
+    @app.route("/<int:year>/<int:month>/<int:day>/<slug>")
+    def view_post(year: int, month: int, day: int, slug: str):
+        try:
+            return render_template(f"blog/{year:04}{month:02}{day:02}_{slug}.html")
+        except TemplateNotFound:
+            return abort(404)
 
     return app
