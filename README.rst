56f119a6b90479bf509a453a02260c7a3b631106
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2023-07-30 14:43:29 +0200

    add compirght notice everywhere!

---
+++ README.rst
@@ -0,0 +1,4 @@
+JSSG - Jtremesay's Static Site Generator
+########################################
+
+The thing that propulse `jtremesay.org <https://jtremesay.org>`_.
--- front/main/cgi.ts
+++ front/main/cgi.ts
@@ -1,3 +1,22 @@
+/*
+ * Computer Generated Images
+ * Copyright (C) 2023 Jonathan Tremesaygues
+ *
+ * cgi.ts
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 function create_triangles(deep: number) {
     if (deep == 0) {
         const path = document.createElementNS("http://www.w3.org/2000/svg", "path")
